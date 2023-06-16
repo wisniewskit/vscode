3474ebba0902eb3d2607895969c02a56e1a896ad
Thomas Wisniewski <wisniewskit@gmail.com>
2023-06-16 10:51:17 -0400

    fix: feature-detect navigator.clipboard.readText properly so Firefox should work when it implements the API (#184958)

--- src/vs/editor/contrib/clipboard/browser/clipboard.ts
+++ src/vs/editor/contrib/clipboard/browser/clipboard.ts
@@ -24,10 +24,9 @@ const CLIPBOARD_CONTEXT_MENU_GROUP = '9_cutcopypaste';
 
 const supportsCut = (platform.isNative || document.queryCommandSupported('cut'));
 const supportsCopy = (platform.isNative || document.queryCommandSupported('copy'));
-// Firefox only supports navigator.clipboard.readText() in browser extensions.
-// See https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/readText#Browser_compatibility
-// When loading over http, navigator.clipboard can be undefined. See https://github.com/microsoft/monaco-editor/issues/2313
-const supportsPaste = (typeof navigator.clipboard === 'undefined' || browser.isFirefox) ? document.queryCommandSupported('paste') : true;
+// navigator.clipboard.readText() is required for pasting. When loading over http,
+// navigator.clipboard can be undefined. See https://github.com/microsoft/monaco-editor/issues/2313
+const supportsPaste = 'clipboard' in navigator && 'readText' in navigator.clipboard;
 
 function registerCommand<T extends Command>(command: T): T {
 	command.register();
